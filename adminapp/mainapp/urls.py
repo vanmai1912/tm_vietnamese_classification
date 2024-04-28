27931f0eb3319aaf9cf6d05f620141f0b440ac5f
vanmai1912 <mai.dv01@gmail.com>
2024-04-28 23:04:43 +0700

    handle api vietnamese_classification

---
+++ adminapp/mainapp/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include, re_path
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register("career-categories", views.CareerCategoryViewSet, "career-category")
+router.register("questions", views.QuestionViewSet, "question")
+router.register("answers", views.AnswerViewSet, "answer")
+router.register("users", views.UserViewSet, 'user')
+router.register("surveys", views.SurveyViewSet, 'survey')
+router.register("universities", views.UniversityViewSet, 'university')
+router.register("feedbacks", views.FeedBackViewSet, 'feedBack')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('oauth2-info/', views.AuthInfo.as_view()),
+    path('answers/vietnamese_classification', views.AnswerViewSet.as_view({'get': 'predict'}), name='vietnamese_classification'),
+    re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
+]
